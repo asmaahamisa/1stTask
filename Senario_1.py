dd24532a85d1c74305d20806381f2a86c2721491
asmaahamisa <asmaahamisa6@gmail.com>
2024-10-28 01:51:02 +0300

    Merged remote master into local master

--- Senario_1.py
+++ Senario_1.py
@@ -16,7 +16,7 @@ options.add_argument("--no-sandbox") #bypass OS security model
 options.add_argument("--disable-dev-shm-usage") #overcome limited resource problems
 options.add_experimental_option("excludeSwitches", ["enable-automation"])
 options.add_experimental_option('useAutomationExtension', False)
-driver = webdriver.Chrome(options=options, executable_path=r'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe')
+driver = webdriver.Chrome(options=options, executable_path=r'C:\Program Files\Google\Chrome\Application\chrome.exe')
 driver.get('http://google.com/')
 #
 
